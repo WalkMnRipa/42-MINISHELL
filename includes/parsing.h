4931dfa13e39a051c9a992f961c8042f9bef10f5
jcohen <jcohen@student.42.fr>
2024-10-01 17:55:23 +0200

    reorganisation

---
+++ includes/parsing.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcohen <jcohen@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 17:46:57 by jcohen            #+#    #+#             */
+/*   Updated: 2024/10/01 17:48:38 by jcohen           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSING_H
+# define PARSING_H
+
+# include "../libft/libft.h"
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+#endif
