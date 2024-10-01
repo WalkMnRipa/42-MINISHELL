e05aeef8d5fc69c285e9f1f32cbf1db0be58559e
jcohen <jcohen@student.42.fr>
2024-10-01 20:02:33 +0200

    Add basic tokenizer
    - Implement token structure and functions
    - Handle basic word, pipe, and redirection tokens
    - Split input on whitespace
    - Recognize '|', '<', '>', '<<' and '>>' as special tokens

--- includes/parsing.h
+++ includes/parsing.h
@@ -6,7 +6,7 @@
 /*   By: jcohen <jcohen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 17:46:57 by jcohen            #+#    #+#             */
-/*   Updated: 2024/10/01 17:48:38 by jcohen           ###   ########.fr       */
+/*   Updated: 2024/10/01 19:59:37 by jcohen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,36 @@
 # define PARSING_H
 
 # include "../libft/libft.h"
+# include <dirent.h>
 # include <fcntl.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <signal.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <sys/ioctl.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <term.h>
+# include <termios.h>
 # include <unistd.h>
+
+typedef enum e_token_type
+{
+	TOKEN_WORD,
+	TOKEN_PIPE,
+	TOKEN_REDIR_INPUT,
+	TOKEN_REDIR_OUTPUT,
+	TOKEN_REDIR_APPEND,
+	TOKEN_HERE_DOC
+}					t_token_type;
+
+typedef struct s_token
+{
+	t_token_type	type;
+	char			*value;
+	struct s_token	*next;
+}					t_token;
 
 #endif
--- src/parsing/cleanup.c
+++ src/parsing/cleanup.c
@@ -1,19 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   cleanup.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jcohen <jcohen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/01 18:16:48 by jcohen            #+#    #+#             */
-/*   Updated: 2024/10/01 19:54:21 by jcohen           ###   ########.fr       */
+/*   Created: 2024/10/01 19:54:50 by jcohen            #+#    #+#             */
+/*   Updated: 2024/10/01 19:55:29 by jcohen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#include "../../includes/parsing.h"
 
-# include "execution.h"
-# include "parsing.h"
+void	free_tokens(t_token *head)
+{
+	t_token	*tmp;
 
-#endif
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->value);
+		free(tmp);
+	}
+}
+
+
