fed01b892287142635210d0fa0543efbaa24f5ae
genarogaribotti <genarogaribotti@hotmail.fr>
2024-10-15 18:10:50 +0200

    commit main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ggaribot <ggaribot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 18:00:00 by ggaribot          #+#    #+#             */
-/*   Updated: 2024/10/15 18:06:39 by ggaribot         ###   ########.fr       */
+/*   Updated: 2024/10/15 18:10:35 by ggaribot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 #include <readline/history.h>
 #include <readline/readline.h>
 
-int			g_signal_received = 0;
+int		g_signal_received = 0;
 
-static void	signal_handler(int signo)
+void	signal_handler(int signo)
 {
 	if (signo == SIGINT)
 	{
@@ -29,34 +29,49 @@ static void	signal_handler(int signo)
 	}
 }
 
-static void	setup_signals(void)
+t_cmd	*tokens_to_cmd(t_token *tokens)
 {
-	signal(SIGINT, signal_handler);
-	signal(SIGQUIT, SIG_IGN);
-}
+	t_cmd	*cmd;
+	int		arg_count;
+	t_token	*current;
+	int		i;
 
-static void	process_input(char *input, t_env **env)
-{
-	t_token	*tokens;
-	t_cmd	*cmd_list;
-
-	tokens = tokenizer(input);
-	if (!tokens)
-		return ;
-	cmd_list = group_tokens_into_commands(tokens);
-	if (!cmd_list)
+	cmd = malloc(sizeof(t_cmd));
+	if (!cmd)
+		return (NULL);
+	arg_count = 0;
+	current = tokens;
+	while (current && current->type != TOKEN_PIPE)
 	{
-		free_tokens(tokens);
-		return ;
+		arg_count++;
+		current = current->next;
 	}
-	execute_command(cmd_list, env);
-	free_tokens(tokens);
-	free_cmd_list(cmd_list);
+	cmd->args = malloc(sizeof(char *) * (arg_count + 1));
+	if (!cmd->args)
+	{
+		free(cmd);
+		return (NULL);
+	}
+	i = 0;
+	current = tokens;
+	while (current && current->type != TOKEN_PIPE)
+	{
+		cmd->args[i] = ft_strdup(current->value);
+		i++;
+		current = current->next;
+	}
+	cmd->args[i] = NULL;
+	cmd->input_fd = STDIN_FILENO;
+	cmd->output_fd = STDOUT_FILENO;
+	cmd->next = NULL;
+	return (cmd);
 }
 
 int	main(int argc, char **argv, char **envp)
 {
 	char	*input;
+	t_token	*tokens;
+	t_cmd	*cmd;
 	t_env	*env;
 
 	(void)argc;
@@ -64,16 +79,18 @@ int	main(int argc, char **argv, char **envp)
 	env = init_env(envp);
 	if (!env)
 		return (1);
-	setup_signals();
 	while (1)
 	{
 		input = readline("minishell> ");
 		if (!input)
 			break ; // EOF (Ctrl+D)
 		if (*input)
-		{
 			add_history(input);
-			process_input(input, &env);
+		tokens = ft_tokenizer(input);
+		if (!tokens)
+		{
+			free(input);
+			continue ;
 		}
 		cmd = tokens_to_cmd(tokens);
 		// This function needs to be implemented by the parsing team
