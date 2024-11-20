b29c9203f30ef82e09a39e414e6aef207142d30e
genarogaribotti <genarogaribotti@hotmail.fr>
2024-11-20 11:22:08 +0100

    ggaribot solve truing the rest

--- src/parsing/quote_handler.c
+++ src/parsing/quote_handler.c
@@ -6,7 +6,7 @@
 /*   By: ggaribot <ggaribot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 18:58:27 by ggaribot          #+#    #+#             */
-/*   Updated: 2024/11/20 00:54:00 by ggaribot         ###   ########.fr       */
+/*   Updated: 2024/11/20 11:15:56 by ggaribot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,16 @@ t_quote_state get_quote_state(char c, t_quote_state current)
     else if (current == STATE_DOUBLE_QUOTE && c == '"')
         return (STATE_NORMAL);
     return (current);
+}
+
+static int should_preserve_quote(char quote_char, t_quote_state outer_state)
+{
+    // Preserve quotes if they're inside the other type of quotes
+    if (outer_state == STATE_SINGLE_QUOTE && quote_char == '"')
+        return (1);
+    if (outer_state == STATE_DOUBLE_QUOTE && quote_char == '\'')
+        return (1);
+    return (0);
 }
 
 static char *process_backslash(char *str, int *i, t_quote_state quote_state)
@@ -61,6 +71,11 @@ char *handle_quotes(char *str, t_env *env)
             result = process_backslash(result, &i, quote_state);
         else if (result[i] == '\'' || result[i] == '"')
         {
+            if (should_preserve_quote(result[i], quote_state))
+            {
+                i++; // Keep the quote and move on
+                continue;
+            }
             quote_state = get_quote_state(result[i], quote_state);
             ft_memmove(&result[i], &result[i + 1], ft_strlen(&result[i]));
             continue;
