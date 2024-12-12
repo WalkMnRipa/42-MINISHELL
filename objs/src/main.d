be092fe5e631a2a826dacb90f426a0ed447ba536
genarogaribotti <genarogaribotti@hotmail.fr>
2024-12-12 18:02:23 +0100

    need to norm tokenizer.c

--- objs/src/main.d
+++
@@ -1,9 +0,0 @@
-objs/src/main.o: src/main.c src/../includes/minishell.h \
- src/../includes/../libft/libft.h src/../includes/../libft/ft_printf.h \
- src/../includes/../libft/get_next_line.h \
- src/../includes/../libft/libft.h
-src/../includes/minishell.h:
-src/../includes/../libft/libft.h:
-src/../includes/../libft/ft_printf.h:
-src/../includes/../libft/get_next_line.h:
-src/../includes/../libft/libft.h:
